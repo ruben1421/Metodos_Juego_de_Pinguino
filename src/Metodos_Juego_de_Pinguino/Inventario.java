37bab36654672ec84d00f0fcbd36874da1586b3a
Hussein <hussein05.ae@alumnes.ilerna.com>
2025-03-27 16:43:51 +0100

    Creación del inventario.java

---
+++ src/Metodos_Juego_de_Pinguino/Inventario.java
@@ -0,0 +1,18 @@
+package Metodos_Juego_de_Pinguino;
+
+public class Inventario {
+	
+	// private static final: un valor constante que es solo accesible a los objetos de clase inventario
+	private static final int maxPeces = 2;
+	private static final int maxBolasNieve = 6;
+
+	public int peces;
+	public int bolasNieve;
+	
+	public Inventario() {
+		this.peces = 0;
+		this.bolasNieve = 0;
+	}
+	
+	
+}
