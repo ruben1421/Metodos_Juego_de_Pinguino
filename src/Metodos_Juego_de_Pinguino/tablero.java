0fc1d9c33da85fe5bd2bb7bd3a9f9f2dc3fe4e3e
DaniMP84 <xDani84x@gmail.com>
2025-03-28 15:52:33 +0100

    Clase tablero

---
+++ src/Metodos_Juego_de_Pinguino/tablero.java
@@ -0,0 +1,18 @@
+package Metodos_Juego_de_Pinguino;
+
+public class tablero {
+    private int numeroDeCasillas;
+    
+    public tablero(int numeroDeCasillas) {
+        this.numeroDeCasillas = numeroDeCasillas;
+    }
+    
+    public void generarTablero() {
+        System.out.println("Generando tablero con " + numeroDeCasillas + " casillas...");
+    }
+    
+    public static void main(String[] args) {
+    	tablero tablero = new tablero(20);
+        tablero.generarTablero();
+    }
+}
