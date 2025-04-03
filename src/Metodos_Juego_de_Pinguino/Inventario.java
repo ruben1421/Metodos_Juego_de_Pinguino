248b18c3613f791c1545f7939b467aa977feb190
Hussein <hussein05.ae@alumnes.ilerna.com>
2025-04-03 16:49:22 +0200

    forgot to do it in my branch

--- src/Metodos_Juego_de_Pinguino/Inventario.java
+++ src/Metodos_Juego_de_Pinguino/Inventario.java
@@ -4,21 +4,27 @@ public class Inventario {
 	
 	// private static final: un valor constante que es solo accesible a los objetos de clase inventario
 	private static final int maxPeces = 2;
+	private static final int maxDados = 3;
 	private static final int maxBolasNieve = 6;
 
 	public int peces;
+	public int dados;
 	public int bolasNieve;
 	
 	public Inventario() {
 		this.peces = 0;
 		this.bolasNieve = 0;
+		this.dados = 0;
 	}
-	
+
 	public int getPeces() {
 		return peces;
 	}
 	public int getBolasNieve() {
 		return bolasNieve;
+	}
+	public int getDados() {
+		return dados;
 	}
 	
 	public void setPeces(int peces) {
@@ -26,5 +32,20 @@ public class Inventario {
 	}
 	public void setBolasNieve(int bolasNieve) {
 		this.bolasNieve = bolasNieve;
+	}
+	public void setDados(int dados) {
+		this.dados = dados;
+	}
+	
+	public void masQueMax() {
+		if(peces >= maxPeces) {
+			peces = maxPeces;
+		}
+		if(bolasNieve >= maxBolasNieve) {
+			bolasNieve = maxBolasNieve;
+		}
+		if(dados >= maxDados) {
+			dados = maxDados;
+		}
 	}
 }
