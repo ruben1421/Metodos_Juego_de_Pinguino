802b1f74fde7014a0d855d5cfd323ac54434563d
DaniMP84 <xDani84x@gmail.com>
2025-04-07 16:47:26 +0200

    Mejorar Clase Casilla y Tablero

--- src/Metodos_Juego_de_Pinguino/Casilla.java
+++ src/Metodos_Juego_de_Pinguino/Casilla.java
@@ -11,23 +11,46 @@ public class Casilla {
 	        this.efecto = efecto;
 	    }
 	    
+	    private String asignarEfectoPorTipo(String tipo) {
+	        switch (tipo) {
+	            case "Oso":
+	                return "retroceder";
+	            case "Trineo":
+	                return "avanzar";
+	            case "Agujero":
+	                return "retroceder";
+	            case "Interrogacion":
+	                return "evento"; // 
+	            default:
+	                return "ninguno";
+	        }
+	    }
 	    public String getTipo() {
 	        return tipo;
 	    }
 	    
-	    public String getEfecto() {
-	        return efecto;
-	    }
-	    
+
 	    public void aplicarEfecto(usuario usuario) {
-	    	if (this.efecto.equals("retroceder")) {
-	    		usuario.retrocederCasillas(2);
-	        } else if (this.efecto.equals("avanzar")) {
-	        	usuario.avanzarCasillas(3); 
-	        } else {
-	            System.out.println("Efecto desconocido");
+	        switch (efecto) {
+	            case "retroceder":
+	                usuario.retrocederCasillas(2);
+	                System.out.println("¡Retrocedes 2 casillas!");
+	                break;
+	            case "avanzar":
+	                usuario.avanzarCasillas(3);
+	                System.out.println("¡Avanzas 3 casillas!");
+	                break;
+	            case "evento":
+	                System.out.println("¡Ocurre un evento aleatorio! (por implementar)");
+	                break;
+	            case "ninguno":
+	                System.out.println("Esta casilla no tiene efecto.");
+	                break;
+	            default:
+	                System.out.println("Efecto desconocido.");
+	                break;
 	        }
 	    }
+	}
 	    
-	    
-}
+
