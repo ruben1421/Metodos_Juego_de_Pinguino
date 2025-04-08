d3bdb82181827654731e335e6712f636e29959a8
david <pino7david@gmail.com>
2025-04-08 19:14:24 +0200

    Correccion varios errores en codigo y puesta en comun de codigo

--- src/Metodos_Juego_de_Pinguino/CasillaOso.java
+++ src/Metodos_Juego_de_Pinguino/CasillaOso.java
@@ -6,12 +6,18 @@ public class CasillaOso extends Casilla {
     }
     
     public void realizarAccion(usuario jugador, Inventario inventario) {
-        if (Inventario.getPeces() > 0) {
-            Inventario.setPeces(Inventario.getPeces() - 1);
+        if (inventario.getCantidadPeces() > 0) {
+            inventario.setCantidadPeces((inventario.getCantidadPeces() - 1));
             System.out.println(jugador.getNombre() + " ha usado un pez para calmar al oso!");
         } else {
             jugador.setPosicion(0);
             System.out.println(jugador.getNombre() + " ha sido atacado por un oso y vuelve al inicio!");
         }
     }
+
+	@Override
+	public void realizarAccion(usuario jugador) {
+		// TODO Auto-generated method stub
+		
+	}
 }
