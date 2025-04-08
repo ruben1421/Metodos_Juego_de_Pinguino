a233c023daa9f57e0d168b5327fa08203fe555aa
Hussein <hussein05.ae@alumnes.ilerna.com>
2025-04-08 16:43:17 +0200

    Mejorando el inventario y creacion del item

--- src/Metodos_Juego_de_Pinguino/Inventario.java
+++ src/Metodos_Juego_de_Pinguino/Inventario.java
@@ -1,51 +1,66 @@
 package Metodos_Juego_de_Pinguino;
 
+import java.util.ArrayList;
+
 public class Inventario {
 	
-	// private static final: un valor constante que es solo accesible a los objetos de clase inventario
-	private static final int maxPeces = 2;
-	private static final int maxDados = 3;
-	private static final int maxBolasNieve = 6;
-
-	public int peces;
-	public int dados;
-	public int bolasNieve;
+	private ArrayList<Item> items; //Un arraylist del clase item
 	
+	//C0nstructor del clase inventario
 	public Inventario() {
-		this.peces = 0;
-		this.bolasNieve = 0;
-		this.dados = 0;
-	}
-
-	public int getPeces() {
-		return peces;
-	}
-	public int getBolasNieve() {
-		return bolasNieve;
-	}
-	public int getDados() {
-		return dados;
+		items = new ArrayList<>(); 
+		
+		//Aqui añadimos al arraylist los items peces, dados, y bolas de nieve
+		items.add(new Item(Item.peces, 0));
+		items.add(new Item(Item.dados, 0));
+		items.add(new Item(Item.bolasNieve, 0));
 	}
 	
-	public void setPeces(int peces) {
-		this.peces = peces;
+	public Item encontrarItem(String nombre) { //Este metodo es para encontrar items y devolverlo
+        for (Item item : items) { //Para cada item del arraylist items
+            if (item.getNombre().equals(nombre)) { //Si el item es igual al nombre, devuelve el item
+                return item;
+            }
+        }
+        return null;
 	}
-	public void setBolasNieve(int bolasNieve) {
-		this.bolasNieve = bolasNieve;
-	}
-	public void setDados(int dados) {
-		this.dados = dados;
-	}
-	
-	public void masQueMax() {
-		if(peces >= maxPeces) {
-			peces = maxPeces;
-		}
-		if(bolasNieve >= maxBolasNieve) {
-			bolasNieve = maxBolasNieve;
-		}
-		if(dados >= maxDados) {
-			dados = maxDados;
-		}
-	}
+    
+    public Item getPeces() { //Devuelve la palabra "Peces"
+        return encontrarItem(Item.peces);
+    }
+    
+    public Item getDados() { //Devuelve la palabra "Dados"
+        return encontrarItem(Item.dados);
+    }
+    
+    public Item getBolasNieve() { //Devuelve la palabra "Bolas de nieve"
+        return encontrarItem(Item.bolasNieve);
+    }
+    
+    public int getCantidadPeces() { //Devuelve la cantidad de peces
+        return getPeces().getCantidad();
+    }
+    public int getCantidadDados() { //Devuelve la cantidad de dados
+        return getDados().getCantidad();
+    }
+    public int getCantidadBolasNieve() { //Devuelve la cantidad de bolas de nieve+
+        return getBolasNieve().getCantidad();
+    }
+    
+    //Cambiar la cantidad de los items individualmente
+    public void setCantidadPeces(int cantidad) {
+        getPeces().setCantidad(cantidad);
+    }
+    public void setCantidadDados(int cantidad) {
+        getDados().setCantidad(cantidad);
+    }
+    public void setCantidadBolasNieve(int cantidad) {
+        getBolasNieve().setCantidad(cantidad);
+    }
+    
+    public void masQueMax() { //Método para garantizar que ningún item exceda su máximo
+        for (Item item : items) {
+            item.setCantidad(item.getCantidad());
+        }
+    }
 }
