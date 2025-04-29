59365ab152816968f44429f76125e98a70776fee
Hussein <hussein05.ae@alumnes.ilerna.com>
2025-04-29 16:28:16 +0200

    Mover carpetas de Pinguino a aqui
    i finally have JavaFX working

---
+++ src/controlador/gestorPartidas.java
@@ -0,0 +1,8 @@
+package controlador;
+
+public class gestorPartidas {
+	private String urlBBDD;
+	private String username;
+	private String password;
+
+}
---
+++ src/controlador/main.java
@@ -0,0 +1,26 @@
+package controlador;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		//System.out.println(getClass().getResource("/pantallaPrincipal.fxml"));
+	    FXMLLoader loader = new FXMLLoader(getClass().getResource("/pantallaPrincipal.fxml"));
+	    Parent root = loader.load();
+
+	    Scene scene = new Scene(root);
+	    primaryStage.setScene(scene);
+	    primaryStage.setTitle("El Juego del Pingüino");
+	    primaryStage.show();
+	}
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
