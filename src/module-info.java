6e1f1e267c3e7457d65b090c043e0a5374777b9a
david <hussein05.ae@alumnes.ilerna.com>
2025-05-07 19:56:18 +0200

    FUNCIONA! en el main.

--- src/module-info.java
+++ src/module-info.java
@@ -1,3 +1,9 @@
 module Metodos_Juego_de_Pinguino {
 	requires java.sql;
+	    requires javafx.controls;
+	    requires javafx.fxml;
+	    
+	    opens vista to javafx.fxml;
+	    exports vista;
+	    exports controlador;
 }
