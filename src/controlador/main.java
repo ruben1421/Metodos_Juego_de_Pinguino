51c9c04d87520fdc122c8be31cd9d8e503148918
Hussein <hussein05.ae@alumnes.ilerna.com>
2025-05-13 16:49:25 +0200

    aumenta el tamaño de las columnas de casillas y anadé comentarios a main

--- src/controlador/main.java
+++ src/controlador/main.java
@@ -9,24 +9,28 @@ import javafx.scene.Scene;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
 
+//Clase principal que inicia el juego
 public class main extends Application {
 
+	//Método que arranca la aplicación
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		//Conecta con la base de datos
 		Connection con = bbdd.conectarBaseDatos();
 		
-		
 		//System.out.println(getClass().getResource("/pantallaPrincipal.fxml"));
+		//Carga la pantalla inicial desde el archivo FXML
 	    FXMLLoader loader = new FXMLLoader(getClass().getResource("/inicio.fxml"));
 	    Parent root = loader.load();
 
+	    //Prepara y muestra la ventana
 	    Scene scene = new Scene(root);
 	    primaryStage.setScene(scene);
-	    primaryStage.setTitle("El Juego del Pingüino");
-	    primaryStage.show();
+	    primaryStage.setTitle("El Juego del Pingüino"); //Título de la ventana
+	    primaryStage.show(); //Abre la ventana
 	}
 
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] args) { //Punto de entrada del programa
+        launch(args); //Inicia la aplicación JavaFX
     }
 }
